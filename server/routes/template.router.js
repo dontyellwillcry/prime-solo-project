87cfd82fc051409330848a6d93f4a4e7cf153a08
LazyDotExe <papaporo@proton.me>
2023-09-03 15:41:58 -0500

    Added a get route for my ingredients table

--- server/routes/template.router.js
+++ server/routes/template.router.js
@@ -6,7 +6,15 @@ const router = express.Router();
  * GET route template
  */
 router.get('/', (req, res) => {
-  // GET route code here
+  const query = `SELECT * FROM ingredients ORDER BY "name" ASC`;
+  pool.query(query)
+    .then( result => {
+      res.send(result.rows);
+    })
+    .catch(err => {
+      console.log('ERROR: Get all ingredients', err);
+      res.sendStatus(500)
+    })
 });
 
 /**
--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
-import {useSelector} from 'react-redux';
+
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 function UserPage() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({ type: "FETCH_INGREDIENT" });
+  }, []);
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
+  const ingredientReducer = useSelector((store) => store.ingredientReducer);
   return (
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
       <p>Your ID is: {user.id}</p>
       <LogOutButton className="btn" />
+      <ul>
+        {ingredientReducer.map((ingredient) => (
+          <li key={ingredient.id}>
+            {ingredient.name} - Type: {ingredient.type}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
