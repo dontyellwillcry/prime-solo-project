6ad930ad1d9e7a2ec89da7e3d8ef00923ddafccc
LazyDotExe <papaporo@proton.me>
2023-09-08 01:22:36 -0500

    Adjusting more material ui in my Admin page.

--- src/components/AdminPage/AdminPage.jsx
+++ src/components/AdminPage/AdminPage.jsx
@@ -2,6 +2,12 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import AdminForm from "./AdminForm";
 import AdminRecipe from "./AdminRecipe";
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
 
 function AdminPage() {
   const dispatch = useDispatch();
@@ -14,8 +20,16 @@ function AdminPage() {
   return (
     <>
     <h1> Admin page</h1>
+    <Container maxWidth="sm">
+    <Grid container rowSpacing={1} columnSpacing={{xs: 1 }}>
+    <Grid item xs={6}>
       <AdminRecipe />
+      </Grid>
+      <Grid item xs={6}>
       <AdminForm />
+      </Grid>
+      </Grid>
+      </Container>
     </>
   );
 }
