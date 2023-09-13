0c44e5bfdbe707e05f10a366fdb3f62dcb9a2047
LazyDotExe <papaporo@proton.me>
2023-09-12 21:52:04 -0500

    Added the dst theme on a toggle for the use to use as they wish

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -27,7 +27,7 @@ function LoginForm() {
   }; // end login
 
   return (
-    <form className="formPanel" onSubmit={login}>
+    <form className="formPanel" onSubmit={login} >
       <h2>Login</h2>
       {errors.loginMessage && (
         <h3 className="alert" role="alert">
--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,18 +1,31 @@
 import React, { useEffect, useState } from "react";
 import UserForm from "./UserForm";
 import { useDispatch, useSelector } from "react-redux";
-
 function UserPage() {
   const dispatch = useDispatch();
+  const audio = new Audio("/sounds/StartScreen_130bpm_3.4time_vinyl.mp3");
+  audio.loop = true;
+  let isMuted = true; // Initially muted
+
+  function toggleAudio() {
+    if (isMuted) {
+      audio.muted = false; // Unmute
+      isMuted = false;
+    } else {
+      audio.muted = true; // Mute
+      isMuted = true;
+    }
+    audio.play();
+  }
 
   useEffect(() => {
     dispatch({ type: "FETCH_INGREDIENT" });
     dispatch({ type: "FETCH_RECIPE" });
-    dispatch({type: "FETCH_FAVORITE"})
+    dispatch({ type: "FETCH_FAVORITE" });
   }, []);
-
   return (
     <div className="background-user">
+      <button onClick={toggleAudio}>Play Music</button>
       <UserForm />
     </div>
   );
